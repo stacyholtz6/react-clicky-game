0cbdb069b77cd893755041e12d5a687c5d76befb
Stacy Holtz <stacyholtz6@gmail.com>
2020-02-23 15:03:55 -0800

    refactored handleIncrement

--- src/App.js
+++ src/App.js
@@ -4,16 +4,6 @@ import Header from './components/Header';
 import Card from './components/Card';
 import images from './images.json';
 
-// To Do:
-
-// Keep track of users score You Win
-// Score +1 if the user clicks an image that hasn't been clicked before
-// Score reset to 0 if user clicks an image more than once
-// Game restarts if score is reset to 0
-// Deploy to gihub pages
-// change style on image cards so they are in two lines
-// fix favicon error - delete or make my own
-
 // shuffleFunction - Fisher Yates Algorithm
 function shuffleFunction(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -40,21 +30,10 @@ class App extends Component {
     this.setState({ images: shuffledImages });
   };
 
-  // handleClick - handles when an image is clicked
-  // handleClick = id => {
-  //   if (this.state.clickedImages.indexOf(id) === -1) {
-  //     this.handleIncrement();
-
-  //     this.setState({ clickedImages: this.state.clickedImages.concat(id) });
-  //   } else {
-  //     this.handleReset();
-  //   }
-  // };
-
-
+  // handClick
   handleClick = id => {
     console.log(id);
-    console.log(this.state.clickedImages)
+    console.log(this.state.clickedImages);
     if (this.state.clickedImages.indexOf(id) === -1) {
       this.handleIncrement();
       var newClickedImages = this.state.clickedImages;
@@ -65,24 +44,22 @@ class App extends Component {
     }
   };
 
-  
   // handleIncrement
   handleIncrement = () => {
     const newScore = this.state.score + 1;
     this.setState({ score: newScore, message: '' });
 
-    // if the score is 12 do this - Need it to stop at 12
-    newScore === 12
-      ? this.setState({
-          message: 'Congratulations, You Win!!',
-          highscore: newScore
-        })
-      : newScore >= this.state.highscore
-      ? this.setState({ highscore: newScore })
-      : this.shuffleCards();
-
-    this.shuffleCards();
-    // this.setState({ restarMessage: '' });
+    if (newScore === 12) {
+      this.setState({
+        message: 'Congratulations, You Win!!',
+        highscore: newScore
+      });
+    } else if (newScore >= this.state.highscore) {
+      this.setState({ highscore: newScore });
+      this.shuffleCards();
+    } else {
+      this.shuffleCards();
+    }
   };
 
   // handleReset - reset the game if user clicks and image twice
@@ -92,15 +69,12 @@ class App extends Component {
       score: 0,
       highscore: this.state.highscore,
       message: 'You clicked an Image Twice! Game Over!!!!!',
-      // restartMessage: 'Click an image to begin',
       clickedImages: []
     });
     this.shuffleCards();
-    // this.setState({ restarMessage: '' });
   };
 
   // map over this.state.images and render an image card for each image in the array
-
   render() {
     return (
       <div>
